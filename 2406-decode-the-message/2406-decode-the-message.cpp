b9cd77e3e125aa41d36d2e6758b725a59d2f0a79
aditibisht22 <114977542+aditibisht22@users.noreply.github.com>
2025-11-14 23:47:34 +0530

    Time: 0 ms (100%), Space: 9.5 MB (90.83%) - LeetHub

---
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        char start = 'a';
+        char mapping[300] = {0};
+        string ans = "";
+
+        for(auto ch: key){
+            if(ch != ' ' && mapping[ch] == 0){
+            mapping[ch] = start;
+            start++;}
+        }
+
+        for(auto ch: message){
+            if(ch == ' ') ans.push_back(' ');
+            else
+            ans.push_back(mapping[ch]);
+
+        }
+        return ans;
+
+    }
+};
