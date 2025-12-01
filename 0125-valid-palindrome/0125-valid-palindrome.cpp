83866bac05040790b7622a9b0549fa51efb6af6f
aditibisht22 <114977542+aditibisht22@users.noreply.github.com>
2025-12-02 02:41:13 +0530

    Time: 0 ms (100%), Space: 10.6 MB (11.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
 
-bool pal(string s){
+bool ispal(string ans){
     int i = 0;
-    int j = s.length()-1;
+    int j = ans.length()-1;
+
     while(i<j){
-        if(s[i] != s[j]){
-            return false;
-        }
-        i++;
-        j--;
+        if(ans[i] != ans[j]) return false;
+         i++;
+         j--;
     }
     return true;
 }
     bool isPalindrome(string s) {
         string ans = "";
         for(auto ch: s){
-            if(isupper(ch))
-            ch = ch + 'a' - 'A';
-
-            if(isalnum(ch)){
-                 ans+= ch;
-            }
+            if(isupper(ch)) ch=ch-'A' +'a';
+            if(isalnum(ch)) ans+=ch;
         }
-        return pal(ans);
+        return ispal(ans);
     }
 };
