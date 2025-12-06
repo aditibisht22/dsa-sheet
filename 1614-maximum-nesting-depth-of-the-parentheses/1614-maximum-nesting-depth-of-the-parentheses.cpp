7792b3d42faeedc78637bf1f10f7d28aa90554bc
aditibisht22 <114977542+aditibisht22@users.noreply.github.com>
2025-12-06 09:22:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (84.38%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<int> st;
-        int result = 0;
+        int count = 0;
+        int maxi = 0;
 
-        for(auto ch : s){
-            if(ch == '(') st.push(ch);
-            else if(ch== ')') st.pop();
-            result = max(int(st.size()), result);
+        for(auto ch: s){
+            if(ch == ')') count--;
+            if(ch == '(') count++;
+            if(count > maxi) maxi =  max(count, maxi);
 
         }
-        return result;
+        return maxi;
     }
 };
