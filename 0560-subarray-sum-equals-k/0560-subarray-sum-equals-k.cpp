bd5ec3a08de0a93c33deaac9215e1f196bdc85bb
Aditi Bisht <114977542+aditibisht22@users.noreply.github.com>
2025-12-17 17:40:14 +0530

    Time: 34 ms (90.36%), Space: 46.9 MB (34.46%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,18 +2,33 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0;
-        int sum;
-        for(int i = 0; i<nums.size(); i++){
-            sum = 0;
-            for(int j = i; j<nums.size(); j++){
-sum+= nums[j];
- if(sum == k){
-            count++;
+        //1. PS array
+        vector<int>ps(nums.size(),0);
+
+        ps[0] = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            ps[i] = ps[i-1] + nums[i];
+        }
+
+        //2. find the count
+        //create hashmap
+        unordered_map<int,int> m;
+        for(int j = 0; j<nums.size(); j++){
+            //1. count how many prefix sum equals k
+            if(ps[j] == k) count++;
+
+            //2. count how many val till j
+            int val = ps[j] - k;
+            if(m.find(val) != m.end()){
+                count+= m[val];
             }
-            
+
+            //populate hashmap
+            if(m.find(ps[j]) == m.end()){
+                m[ps[j]] = 0;
+            }
+            m[ps[j]]++;
         }
-        }
-       
         return count;
     }
 };
