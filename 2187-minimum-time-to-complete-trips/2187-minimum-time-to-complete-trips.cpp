4672fd41e44514c5b6865cb13f9d13c455650819
Aditi Bisht <114977542+aditibisht22@users.noreply.github.com>
2025-12-20 05:40:14 +0530

    Time: 47 ms (70.81%), Space: 98.5 MB (51.51%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
 
-     bool possible(vector<int>& time, long long givenTime, int totalTrips) {
-        long long actualTrips = 0;
-        
-        for(int &t : time) {
-            actualTrips += givenTime/t;
+    bool possible(vector<int>& time, long long mid, int totalTrips){
+        long long result = 0;
+        for(auto &it: time){
+            result+= mid/it;
         }
-        
-        return actualTrips >= totalTrips;
+        return (result >= totalTrips);
+
     }
     
+    int mini(vector<int>& time){
+        int min_element = INT_MAX;
+        for(int i = 0; i<time.size(); i++){
+            min_element = min(min_element, time[i]);
+        }
+        return min_element;
+    }
     long long minimumTime(vector<int>& time, int totalTrips) {
    
         long long s = 1;
-        long long e = (long long) *min_element(time.begin(),time.end()) * totalTrips;
+        long long e = (long long) mini(time) * totalTrips;
 
         while(s<e){
             long long mid = s+(e-s)/2;
