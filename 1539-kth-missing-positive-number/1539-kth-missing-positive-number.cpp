fd894c0e6b9cdffd2fb53f7065c3c7556a699e5d
Aditi Bisht <114977542+aditibisht22@users.noreply.github.com>
2025-12-24 21:09:00 +0530

    Time: 0 ms (100%), Space: 13.3 MB (58.06%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
         
-        //2. brute force
-        int i = 0;
-        int num = 1;
+        int l = 0, r = n-1;
         
-        while(i < arr.size() && k > 0){
-            if(arr[i] == num) i++;
-            else k--;
-
-            num++;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(arr[mid] - (mid+1) < k) { //A[mid]-(mid+1)   --> This gives umber of missing number before m'th index
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+            
         }
-
-        while(k--){
-            num++;
-        }
-        return num-1;
+        
+        
+        return l + k; 
     }
 };
