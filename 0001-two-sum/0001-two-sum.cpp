7e03bc4faa46c993c87e60b8b9ba090999e336f0
Aditi Bisht <114977542+aditibisht22@users.noreply.github.com>
2026-02-02 22:23:52 +0530

    Time: 29 ms (38.06%), Space: 14 MB (80.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         for(int i =0; i<n; i++){
             for(int j = i+1; j<n; j++){
-                if(nums[i] + nums[j] == target){
+                if(target - nums[i] == nums[j]){
                     return {i,j};
                 }
             }
